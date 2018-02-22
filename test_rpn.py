6ea794e9171010585cbee30eb2818f30417f33db
shiyuwu <shiyuwu@umich.edu>
2018-02-22 14:38:00 -0500

    Add exponent test

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,5 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+		result_exponent = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result_exponent)
