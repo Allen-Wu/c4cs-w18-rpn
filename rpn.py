fbbfa9fba8c794bdad4eae52a9b5372161d15325
shiyuwu <shiyuwu@umich.edu>
2018-02-22 14:45:07 -0500

    Add exponent func

--- rpn.py
+++ rpn.py
@@ -7,9 +7,17 @@ def calculate(arg):
 			value = int(token)
 			stack.append(token)
 		except ValueError:
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			return int(arg1) + int(arg2)
+			if value == '+':
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				return int(arg1) + int(arg2)
+			if value == '^'
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = 1
+				for x in range(1, arg2)
+					result = result * arg1
+				return result
 def main():
 	while True:
 		print(calculate(input("rpn calc> ")))
